f428b181341255cb3d78d760df1312e68445e6c6
hpsprger <hpsprger@gmail.com>
2020-07-11 15:01:01 +0800

    add debug info

--- uboot-imx/build.sh
+++ uboot-imx/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #make mx6qsabresd_config
+echo step1==========================================
 make mx6qsabresdandroid_config
+echo step2==========================================
 #make V=1 -w -j8 
 make  -j8 
+echo all done==========================================
+
