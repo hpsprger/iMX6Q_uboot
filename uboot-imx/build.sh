1bda821473c1c4f5462e04cdf5d63d2cb8fe9b25
hpsprger <hpsprger@gmail.com>
2020-07-24 08:15:37 +0800

    mdf

--- uboot-imx/build.sh
+++ uboot-imx/build.sh
@@ -2,6 +2,12 @@
 #make mx6qsabresd_config
 echo =============================step1==========================================
 make mx6qsabresdandroid_config
+echo 
+echo 
+echo 
+echo 
+echo 
+echo 
 echo =============================step2==========================================
 #make V=1 -w -j8 
 make  -j8 
